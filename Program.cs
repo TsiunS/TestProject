901efd767a0236e9735dd1c2c03860beae7e0bdc
Sergey Tsiunel <tsiunels@yandex.ru>
2026-01-18 20:04:16 +0400

    testCacheFile added

--- Program.cs
+++ Program.cs
@@ -8,14 +8,14 @@
 
             var cacheFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testCacheFile.txt");
 
-           var dir1 = Directory.CreateDirectory("TestFolder1");
+           var dir1 = Directory.CreateDirectory("TestFolder2");
 
-           var subdir1 = dir1.CreateSubdirectory("SubFolder1");
-            var subdir2 = dir1.CreateSubdirectory("SubFolder2");
+           var subdir1 = dir1.CreateSubdirectory("SubFolder2");
+            var subdir2 = dir1.CreateSubdirectory("SubFolder3");
 
-            subdir1.CreateSubdirectory("SubsubFolder3");
+            subdir1.CreateSubdirectory("SubsubFolder4");
 
-            File.WriteAllText(Path.Combine(subdir1.FullName, "File.txt"), "Test content");
+            File.WriteAllText(Path.Combine(subdir1.FullName, cacheFilePath), "Test content");
 
             Console.ReadKey();
         }
