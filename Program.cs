8387b5bec403933235c8591113ac3c6274a2586f
Sergey Tsiunel <tsiunels@yandex.ru>
2026-01-18 19:57:19 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var cacheFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testCacheFile.txt");
+
+           var dir1 = Directory.CreateDirectory("TestFolder1");
+
+           var subdir1 = dir1.CreateSubdirectory("SubFolder1");
+            var subdir2 = dir1.CreateSubdirectory("SubFolder2");
+
+            subdir1.CreateSubdirectory("SubsubFolder3");
+
+            File.WriteAllText(Path.Combine(subdir1.FullName, "File.txt"), "Test content");
+
+            Console.ReadKey();
+        }
+    }
+}
