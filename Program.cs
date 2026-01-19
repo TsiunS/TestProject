807d5bfbfaea8a18917e47b8479646ec70d943f4
Sergey Tsiunel <tsiunels@yandex.ru>
2026-01-19 19:03:48 +0400

    change no 8

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@
         {
             Console.WriteLine("Hello, World!");
 
-            var cacheFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testCacheFile.txt");
+           // var cacheFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testCacheFile.txt");
 
-           var dir1 = Directory.CreateDirectory("TestFolder2");
+           //var dir1 = Directory.CreateDirectory("TestFolder2");
 
-           var subdir1 = dir1.CreateSubdirectory("SubFolder2");
-            var subdir2 = dir1.CreateSubdirectory("SubFolder3");
+           //var subdir1 = dir1.CreateSubdirectory("SubFolder2");
+           // var subdir2 = dir1.CreateSubdirectory("SubFolder3");
 
-            subdir1.CreateSubdirectory("SubsubFolder4");
+           // subdir1.CreateSubdirectory("SubsubFolder4");
 
-            File.WriteAllText(Path.Combine(subdir1.FullName, cacheFilePath), "Test content");
+           // File.WriteAllText(Path.Combine(subdir1.FullName, cacheFilePath), "Test content");
 
             Console.ReadKey();
         }
