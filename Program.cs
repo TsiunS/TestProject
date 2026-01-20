6202599c55b946c90c6ef1715040a2fc6d1f0b4f
Sergey Tsiunel <tsiunels@yandex.ru>
2026-01-20 16:36:56 +0400

    new folder 2added

--- Program.cs
+++ Program.cs
@@ -6,16 +6,18 @@
         {
             Console.WriteLine("Hello, World!");
 
-           // var cacheFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testCacheFile.txt");
+            var cacheFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testCacheFile222.txt");
 
-           //var dir1 = Directory.CreateDirectory("TestFolder2");
+           var dir1 = Directory.CreateDirectory("TestFolder222");
 
-           //var subdir1 = dir1.CreateSubdirectory("SubFolder2");
-           // var subdir2 = dir1.CreateSubdirectory("SubFolder3");
+           var subdir1 = dir1.CreateSubdirectory("SubFolder44");
+            // var subdir2 = dir1.CreateSubdirectory("SubFolder3");
 
-           // subdir1.CreateSubdirectory("SubsubFolder4");
+            // subdir1.CreateSubdirectory("SubsubFolder4");
 
-           // File.WriteAllText(Path.Combine(subdir1.FullName, cacheFilePath), "Test content");
+            File.WriteAllText(Path.Combine(dir1.FullName, "AAAAAAA.txt"), "Test content44444444444");
+
+            File.WriteAllText(Path.Combine(subdir1.FullName, "Subfile22222.txt"), "Test content222");
 
             Console.ReadKey();
         }
