c4b52d5214e6997e07a188bd7a77b3360d0b47c3
Sergey Tsiunel <tsiunels@yandex.ru>
2026-01-20 21:05:59 +0400

    test change 11

--- Program.cs
+++ Program.cs
@@ -8,16 +8,16 @@
 
             var cacheFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testCacheFile222.txt");
 
-           var dir1 = Directory.CreateDirectory("TestFolder222");
+           var dir1 = Directory.CreateDirectory("TestFolder333");
 
-           var subdir1 = dir1.CreateSubdirectory("SubFolder44");
+           var subdir1 = dir1.CreateSubdirectory("SubFolder5555");
             // var subdir2 = dir1.CreateSubdirectory("SubFolder3");
 
             // subdir1.CreateSubdirectory("SubsubFolder4");
 
-            File.WriteAllText(Path.Combine(dir1.FullName, "AAAAAAA.txt"), "Test content44444444444");
+            File.WriteAllText(Path.Combine(dir1.FullName, "AAAASSSSAAA.txt"), "Test content44444444444");
 
-            File.WriteAllText(Path.Combine(subdir1.FullName, "Subfile22222.txt"), "Test content222");
+            File.WriteAllText(Path.Combine(subdir1.FullName, "Subfile2228822.txt"), "Test content222");
 
             Console.ReadKey();
         }
