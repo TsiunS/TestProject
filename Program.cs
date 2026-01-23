3a376981f44bbd4f16d3c0e2828ac9d5b28a67be
Sergey Tsiunel <tsiunels@yandex.ru>
2026-01-23 15:51:47 +0400

    testFile added

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main(string[] args)
         {
+
+            
+
+
+
             Console.WriteLine("Hello, World!");
 
             var cacheFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testCacheFile222.txt");
@@ -21,5 +26,46 @@
 
             Console.ReadKey();
         }
+
+      
+
+
+        public static List<Tuple<string, DateTime>> GetFilesWithCreateDates(string baseDirectory)
+        {
+            List<Tuple<string, DateTime>> result = new List<Tuple<string, DateTime>>();
+
+            // Получаем массив путей ко всем файлам в директории и её подпапках
+            string[] allFiles = Directory.GetFiles(baseDirectory, "*.*", SearchOption.AllDirectories);
+
+            for (int i = 0; i < allFiles.Length; i++)
+            {
+                string fullFilePath = allFiles[i];
+                string relativePath = fullFilePath.Substring(baseDirectory.Length);
+
+                // Удаляем возможный ведущий символ "/"
+                if (relativePath.StartsWith("\\\\") || relativePath.StartsWith("/"))
+                {
+                    relativePath = relativePath.Substring(1);
+                }
+
+                try
+                {
+                    // Запрашиваем дату создания файла
+                    DateTime creationTimeUtc = File.GetCreationTimeUtc(fullFilePath);
+
+                    // Добавляем кортеж в результирующий список
+                    Tuple<string, DateTime> tupleItem = new Tuple<string, DateTime>(relativePath, creationTimeUtc);
+                    result.Add(tupleItem);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка при обработке файла " + fullFilePath + ": " + ex.Message);
+                }
+            }
+
+            return result;
+        }
+    
+
     }
 }
